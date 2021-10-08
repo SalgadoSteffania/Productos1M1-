8e525f57e540fc65451b258ce29ed1967c2df014
Margie Fuertes <85895396+MFuertes1012@users.noreply.github.com>
2021-10-08 12:21:09 -0600

    Add files via upload

---
+++ Inventario-main/ProductosApp/Formularios/MetodosInventario/FrmEntrada.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Infraestructure.Productos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductosApp.Formularios.MetodosInventario
+{
+    public partial class FrmEntrada : Form
+    {
+        public ProductoModel ProductoModel;
+        public int Count { get; set; }
+        public FrmEntrada()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Producto p = new Producto()
+            {
+                Id = ++Count,
+                Nombre = txtNombre.Text,
+                Descripcion = txtDesc.Text,
+                Existencia = (int)nudExist.Value,
+                Precio = nudPrice.Value,
+                FechaVencimiento = dtpCaducity.Value,
+                FechaDeRegistro = dtpFechaRegistro.Value,
+                UnidadMedida = (UnidadMedida)cmbMeasureUnit.SelectedIndex,
+                Naturaleza = Domain.Enums.NaturalezaCuentas.Entrada
+            };
+
+            ProductoModel.Add(p);
+            Dispose();
+        }
+    }
+}
