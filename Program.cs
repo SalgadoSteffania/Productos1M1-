7c5d5da0f748558efe3048e4dda2877bafd04b31
SalgadoSteffania <86125323+SalgadoSteffania@users.noreply.github.com>
2021-10-07 23:46:15 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using AppCore.Interfaces;
+using AppCore.Services;
+using Autofac;
+using Domain.Interfaces;
+using Infraestructure.Empleados;
+using Infraestructure.Productos;
+using ProductosApp.Formularios;
+using System;
+using System.Windows.Forms;
+
+
+
+namespace ProductosApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<EmpleadoModel>().As<IEmpleadoModel>();
+            builder.RegisterType<EmpleadoService>().As<IEmpleadoService>();
+
+            var container = builder.Build();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmGestionEmpleados(container.Resolve<IEmpleadoService>()));
+
+            builder.RegisterType<ProductoModel>().As<IProductoModel>();
+
+
+
+
+        }
+    }
+}
